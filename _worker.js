d6a603c42d74ec788bc30184af2ec9f55ad33046
Mahoor123456 <hamidbabaey1359@gmail.com>
2024-11-30 02:12:29 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ada2f289-fed2-4da6-959f-3a4162c19061';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
